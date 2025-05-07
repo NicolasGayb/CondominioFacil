a57f91f6bfdb8129845d951c2e9faaa7a2b3e28f
Nicolas Gabriel <nicolasgbleal@gmail.com>
2025-05-07 18:36:42 -0300

    React

---
+++ src/main/java/com/condominio/CondominioFacilApplication.java
@@ -0,0 +1,14 @@
+package com.condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal do Spring Boot que inicia a aplicação.
+ */
+@SpringBootApplication
+public class CondominioFacilApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CondominioFacilApplication.class, args);
+    }
+}
