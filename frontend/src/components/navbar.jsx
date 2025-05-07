9ce97b837918f834e2600f73af05416a2e5970c7
Nicolas Gabriel <nicolasgbleal@gmail.com>
2025-05-07 17:41:33 -0300

    React navbar

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Condômino Fácil</h1>
+            <div>
+                <a href="/login">Entrar</a>
+                <a href="/register">Registrar</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
