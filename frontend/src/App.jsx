f1468080d02e008e96bfd8e6f3ecd08861712695
Nicolas Gabriel <nicolasgbleal@gmail.com>
2025-05-16 17:35:45 -0300

    teste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Login from "./components/login";
 import Register from "./components/register";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
-      <Route path="/" element={<Home />} />           {/* Home na rota raiz */}
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
