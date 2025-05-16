bef40a4f7c5c54958152e9d125cf8b3a8dedc02b
Nicolas Gabriel <nicolasgbleal@gmail.com>
2025-05-16 17:12:48 -0300

    ajuste front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/home"; 
-import Login from "./components/login"; 
+import Home from "./components/home";
+import Login from "./components/login";
 import Register from "./components/register";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />} />           {/* Home na rota raiz */}
-      <Route path="/login" element={<Login />} />     {/* Login separado */}
-      <Route path="/register" element={<Register />} /> {/* Cadastro, se houver */}
+      <Route path="/" element={<Home />} />           {/* Home na rota raiz */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </BrowserRouter>
 );
