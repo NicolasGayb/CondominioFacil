1b054c9eb7ff82a3fde7621b80cb32f86fee152e
NicolasGayb <nicolasgbleal@gmail.com>
2025-05-18 17:53:50 -0300

    finalização backend

--- web/src/main/java/com/condominio/config/CorsConfig.java
+++ web/src/main/java/com/condominio/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.condominio.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
